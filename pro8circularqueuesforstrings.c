0bdb759fe91123e3251c14bf4ed3b53a6e0789eb
Dheemanth-SM-07 <153764968+Dheemanth-SM-07@users.noreply.github.com>
2024-02-14 22:40:34 +0530

    Add files via upload
    circular queues for strings

---
+++ pro8circularqueuesforstrings.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX_SIZE 3
+
+struct circularqueue
+{
+    int data;
+};
+struct circularqueue queue[MAX_SIZE];
+int front=-1,rear=-1;
+void insert(int value);
+int delete();
+void display();
+int main()
+{
+    int choice,value;
+    while(1)
+    {
+        printf("1.Insert element into the circular queue\n");
+        printf("2.Delete element from the circular queue\n");
+        printf("3.Display the circular queue\n");
+        printf("4.Exit\n");
+        printf("Enter your choice\n");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+                  printf("Enter the value to be inserted\n");
+                  scanf("%d",&value);
+                  insert(value);
+                  break;
+            case 2:
+                 value=delete();
+                 if(value!=-1)
+                 {
+                    printf("The deleted element is : %d\n",value);
+                 }
+                 break;
+            case 3:
+                  display();
+                  break;
+            case 4:
+                exit(0);
+                break;
+            default:
+                 printf("Invalid choice\n");
+                 break;
+        }
+    }
+    return 0;
+}
+void insert(int value)
+{
+    if((rear + 1) % MAX_SIZE == front)
+    {
+        printf("Circular Queue overflow\n");
+        return;
+    }
+    rear = (rear + 1) % MAX_SIZE;
+    queue[rear].data = value;
+    if(front == -1)
+    {
+        front = rear;
+    }
+}
+int delete()
+{
+    int value;
+    if(front==-1)
+    {
+        printf("Circular queue underflow\n");
+        return -1;
+    }
+    value=queue[front].data;
+    if(front==rear)
+    {
+      front=rear=-1;
+     }
+     else
+     {
+        front=(front+1)%MAX_SIZE;
+     }
+     return value;
+}
+void display()
+{
+    int i;
+    if(front==-1)
+    {
+        printf("\n Empty Queue\n");
+    }
+    else{
+        printf("\n front-> %d",front);
+        printf("\n items-> ");
+        for(i=front;i!=rear;i=(i+1)%MAX_SIZE)
+        {
+            printf("%d\t",queue[i].data);
+        }
+        printf("%d",queue[i].data);
+        printf("\n Rear -> %d \n",rear);
+    }
+
+}
